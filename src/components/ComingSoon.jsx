e1101da9d81f147e3fca0019aa183904ff1c3ca0
annguyen1909 <nguyentruongan1419@gmail.com>
2024-08-26 15:45:26 +0700

    Update ComingSoon.jsx

--- src/components/ComingSoon.jsx
+++ src/components/ComingSoon.jsx
@@ -1,7 +1,7 @@
 const ComingSoon = () => {
     return (
       <>
-        <div className="relative min-h-[80vh] flex flex-col  place-items-center ">
+        <div className="relative min-h-[80vh] top-40 flex flex-col  place-items-center ">
           <h2 className="text-center font-heading m-10 text-6xl sm:text-7xl lg:text-8xl leading-[5rem] sm:leading-[7rem] lg:leading-[7rem] font-black	 ">
             <span className="bg-clip-text text-transparent bg-gradient-to-r from-pink-500 to-violet-500">
               Coming Soon
